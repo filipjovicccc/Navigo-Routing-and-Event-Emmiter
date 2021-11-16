d1dfe9be0878cfc0a90b891a5dc37405aae5f2cc
johnloccck <filipjovicccc@gmail.com>
2021-11-16 22:53:42 +0100

    update

--- src/components/TrashEmitter.jsx
+++ src/components/TrashEmitter.jsx
@@ -1,32 +1,39 @@
 import { h } from "preact";
 import { useState, useEffect } from "preact/hooks";
 import { Button } from "../shared/sharedComponents";
-const getTodosFromLocalStorage = JSON.parse(localStorage.getItem("todos"));
 
-const filteredList = (list, listName) =>
-  list.filter((todo) => todo.list === listName);
-
-function TrashEmitter(id) {
+function TrashEmitter() {
+  const getTodosFromLocalStorage = JSON.parse(localStorage.getItem("todos"));
   const [logs, setLogs] = useState(getTodosFromLocalStorage);
 
   useEffect(() => {
-    const filtered = filteredList(getTodosFromLocalStorage, 'DELETED')
-    setLogs(filtered)
-  }, []);
+    const filtered = async () =>
+      filteredList(getTodosFromLocalStorage, "DELETED");
+    setLogs(filtered);
 
+    return () => {};
+  }, [getTodosFromLocalStorage]);
+
+  const filteredList = (list, listName) =>
+    list.filter((todo) => todo.list === listName);
+  console.log(logs);
   const deleteBtn = (id) => {
     setLogs(logs.filter((t) => t.id !== id));
   };
 
-  console.log('[out]from local: ', getTodosFromLocalStorage, '[out]logs: ', logs);
   return (
     <div>
-      {logs.length !== 0 && logs.map((log) => (
-        <div className="todo" key={log.key}>
-          {log.text}
-          <Button fn={() => deleteBtn(log.id)} customClass="trash-btn" arrow="fas fa-trash" />
-        </div>
-      ))}
+      {logs.length !== 0 &&
+        logs.map((log) => (
+          <div className="todo" key={log.key}>
+            {log.text}
+            <Button
+              fn={() => deleteBtn(log.id)}
+              customClass="trash-btn"
+              arrow="fas fa-trash"
+            />
+          </div>
+        ))}
     </div>
   );
 }
